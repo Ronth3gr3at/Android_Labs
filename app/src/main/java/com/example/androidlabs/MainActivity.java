40a4ad4244bf82244d355514b07cdfa21434b078
Ronth3gr3at <tava0031@algonquinlive.com>
2020-01-31 22:44:05 -0500

    LAB 2 - Changes - Relative, Grid, and Linear Layouts created

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,51 +2,80 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences sharedPref;//init shared preferences to null
+    private SharedPreferences.Editor editPref;
+    private EditText editEmail, editPassword;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
-        //BUTTON
-        Button btn = findViewById(R.id.button);
-        btn.setOnClickListener( v -> {Toast.makeText(getApplicationContext(),"Here is more information", Toast.LENGTH_LONG).show();});
+        setContentView(R.layout.activity_main_lab3); //init layout lab3
+        editEmail = findViewById(R.id.email);
+        editPassword = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
 
-        //CHECKBOX
-       CheckBox chk = findViewById(R.id.checkbox);
-        chk.setOnCheckedChangeListener((CompoundButton check, boolean state) -> {
-            String message;
-            if (state){
-                message = "on";
-            } else {
-                message = "off";
-            }
-            Snackbar.make(chk,"The checkbox is: " + message,Snackbar.LENGTH_LONG).setAction("undo", click -> check.setChecked(!state)).show();
+        sharedPref = getSharedPreferences("com.example.androidlabs.lab3", Context.MODE_PRIVATE);
+        editPref = sharedPref.edit();
+        editPref.putString("email", " ");
 
-        });
-        //SWITCH
-       Switch swi = findViewById(R.id.Switch);
-        swi.setOnCheckedChangeListener( (CompoundButton swtch, boolean state) -> {
-            String message;
-            if (state){
-                message = "on";
-            } else {
-                message = "off";
-            }
-            Snackbar.make(swtch,"The switch is: " + message,Snackbar.LENGTH_LONG).setAction("undo", click -> swtch.setChecked(!state)).show();
+        checkSharedPreferences();
+        loginButton.setOnClickListener( clickListener -> {
 
+                String email = editEmail.getText().toString();
+                String password = editPassword.getText().toString();
+
+                //save email
+                editPref.putString(getString(R.string.email), email);
+                editPref.commit();
+
+                //save password
+                editPref.putString(getString(R.string.password), password);
+                editPref.commit();
+
+                Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+                goToProfile.putExtra("EMAIL", email);
+                startActivity(goToProfile);
         });
     }
 
+    private void checkSharedPreferences(){
+        String email = sharedPref.getString(getString(R.string.email),"");
+        String password = sharedPref.getString(getString(R.string.password), "");
 
+        editEmail.setText(email);
+        editPassword.setText(password);
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
 
 }
---
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -0,0 +1,103 @@
+package com.example.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class ProfileActivity extends AppCompatActivity {
+private ImageButton imageButton;
+private static final int REQUEST_IMAGE_CAPTURE = 1;
+static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+private String functionName;
+private EditText emailEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //init
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        //VIEWS
+        imageButton = findViewById(R.id.image_button);
+        emailEditText = findViewById(R.id.email_profile);
+
+        //OnClick Listeners
+        imageButton.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+
+        Intent fromMain = getIntent();
+
+        //change view text
+        emailEditText.setText(fromMain.getStringExtra("EMAIL"));
+
+        //LOG
+        functionName = "onCreate";
+        Log.e(ACTIVITY_NAME, "In function: " + functionName);
+    }
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+        functionName = "dispatchTakePictureIntent";
+        Log.e(ACTIVITY_NAME, "In function: " + functionName);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageButton.setImageBitmap(imageBitmap);
+        }
+        functionName = "onActivityResult";
+        Log.e(ACTIVITY_NAME, "In function: " + functionName);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        functionName = "onStart";
+        Log.e(ACTIVITY_NAME, "In function: " + functionName);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        functionName = "onResume";
+        Log.e(ACTIVITY_NAME, "In function: " + functionName);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        functionName = "onPause";
+        Log.e(ACTIVITY_NAME, "In function: " + functionName);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        functionName = "onStop";
+        Log.e(ACTIVITY_NAME, "In function: " + functionName);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        functionName = "onDestroy";
+        Log.e(ACTIVITY_NAME, "In function: " + functionName);
+    }
+}
